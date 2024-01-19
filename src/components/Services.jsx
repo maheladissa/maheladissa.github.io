ad17890553ff600693b2c2151df778b2d3660d80
MahelaDissa <maheladissanayake@gmail.com>
2024-01-19 23:18:41 +0530

    Changed Services component to use Flexbox insted of grid for beter look

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -30,11 +30,11 @@ const Services = () => {
             title={"Services 🛠️"}
             subtitle={"Here are the services I provide to my clients"}
         >
-            <div className={"grid gap-10 grid-cols-12 justify-center m-auto"}>
+            <div className={"flex flex-wrap gap-10 justify-center m-auto"}>
                 {
                     SERVICES.map(SERVICES => (
-                        <div key={SERVICES.id} className={"flex flex-col items-center justify-center p-5 shadow-lg dark:shadow-gray-400 rounded-xl duration-300 ease-in-out hover:scale-110 col-span-12 md:col-span-6 lg:col-span-4"}>
-                            <img src={SERVICES.image} alt={SERVICES.title} className={"h-36 md:h-44 object-contain"} />
+                        <div key={SERVICES.id} className={"flex flex-col items-center justify-center p-5 shadow-lg dark:shadow-gray-400 rounded-xl duration-300 ease-in-out hover:scale-110"}>
+                            <img src={SERVICES.image} alt={SERVICES.title} className={"h-44 object-contain"} />
                             <h3 className={"mt-5 text-base"}>{SERVICES.title}</h3>
                         </div>
                     ))
