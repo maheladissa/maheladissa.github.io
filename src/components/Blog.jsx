7077ca99313346d1e851d8341f5e4eca167f2fda
MahelaDissa <maheladissanayake@gmail.com>
2024-05-31 17:21:08 +0530

    Fixed axios not found error

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Section from "./common/Section";
 import blog_DBSecurity from "../assets/Blog_DBSecurity.webp";
 import blog_IDEval from "../assets/Blog_IDEval.webp";
